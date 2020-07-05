2a5e6fe866df38a219baae13df357b8edcf66be9
Tom <40807906+tlan2@users.noreply.github.com>
2020-07-05 14:27:08 -0700

    Added Command Line Interface description that passes corresponding test.

---
+++ phonebill/src/it/java/edu/pdx/cs410J/tlan2/Project1IT.java
@@ -0,0 +1,52 @@
+package edu.pdx.cs410J.tlan2;
+
+import edu.pdx.cs410J.InvokeMainTestCase;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Tests the functionality in the {@link Project1} main class.
+ */
+public class Project1IT extends InvokeMainTestCase {
+
+    /**
+     * Invokes the main method of {@link Project1} with the given arguments.
+     */
+    private MainMethodResult invokeMain(String... args) {
+        return invokeMain( Project1.class, args );
+    }
+
+  /**
+   * Tests that invoking the main method with no arguments issues an error
+   */
+  @Test
+  public void testNoCommandLineArguments() {
+    MainMethodResult result = invokeMain();
+    assertThat(result.getExitCode(), equalTo(1));
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  }
+
+  @Test
+    public void implementCorrectCommandLineInterfaceDescription(){
+      MainMethodResult result = invokeMain(Project1.class);
+      assertThat(result.getTextWrittenToStandardOut(), containsString(
+              "\nusage: java edu.pdx.cs410J.<login-id>.Project1 [options] <args>" +
+                      "\n  args are (in this order):" +
+                      "\n\tcustomer\t\t\tPerson whose phone bill we're modeling" +
+                      "\n\tcallerNumber\t\tPhone number of caller" +
+                      "\n\tcalleeNumber\t\tPhone number of person who was called" +
+                      "\n\tstart\t\t\t\tDate and time call began (24-hour time)"+
+                      "\n\tend\t\t\t\t\tDate and time call ended (24-hour time)" +
+                      "\n  options are (options may appear in any order) :" +
+                      "\n\t-print\t\t\t\tPrints a description of the new phone call" +
+                      "\n\t-README\t\t\t\tPrints a README for this project and exits" +
+                      "\n  Date and time should be in the format: mm/dd/yyyy hh:mm"
+      ));
+  }
+
+
+
+}
