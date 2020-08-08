f95a974329f795fb62c57b744c2f7074dd97c452
Tom <40807906+tlan2@users.noreply.github.com>
2020-08-08 14:47:47 -0700

    Add a new activity for calculator. It's empty right now.

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/tlan2/phonebill/CalculatorActivity.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410j.tlan2.phonebill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
--- phonebill-android/app/src/main/java/edu/pdx/cs410j/tlan2/phonebill/MainActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/tlan2/phonebill/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs410j.tlan2.phonebill;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,10 +9,13 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button searchPhoneBill = findViewById(R.id.searchPhoneBill);
+        searchPhoneBill.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+//                Log.i("MainActivity", "The button was clicked");
+//                Toast.makeText(MainActivity.this, "I was clicked", Toast.LENGTH_LONG).show(); // "I was clicked" appears for a bit
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
