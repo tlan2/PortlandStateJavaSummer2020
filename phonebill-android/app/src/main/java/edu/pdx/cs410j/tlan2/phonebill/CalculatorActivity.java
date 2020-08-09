3a42365834bcb9b811ed8d9ccf0013e1051dc86d
Tom <40807906+tlan2@users.noreply.github.com>
2020-08-08 20:30:57 -0700

    Added the ability to pass the result of the CalculatorActivity back to the MainActivity.

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/tlan2/phonebill/CalculatorActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/tlan2/phonebill/CalculatorActivity.java
@@ -2,13 +2,49 @@ package edu.pdx.cs410j.tlan2.phonebill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    private double sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        Button execute = findViewById(R.id.execute);
+        execute.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View view) {
+                EditText leftOperand = findViewById(R.id.leftOperand);
+                EditText rightOperand = findViewById(R.id.rightOperand);
+
+                double left = Double.parseDouble(leftOperand.getText().toString());
+                double right = Double.parseDouble(rightOperand.getText().toString());
+
+                sum = left + right;
+
+                TextView result = findViewById(R.id.result);
+                result.setText(String.valueOf(sum));
+            }
+        });
+
+        Button backToMain = findViewById(R.id.backToMain);
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CalculatorActivity.this, MainActivity.class);
+                intent.putExtra("Sum", sum);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
--- phonebill-android/app/src/main/java/edu/pdx/cs410j/tlan2/phonebill/MainActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/tlan2/phonebill/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -19,6 +20,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int CALCULATOR_RESULT = 43;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +34,21 @@ public class MainActivity extends AppCompatActivity {
 //                Log.i("MainActivity", "The button was clicked");
 //                Toast.makeText(MainActivity.this, "I was clicked", Toast.LENGTH_LONG).show(); // "I was clicked" appears for a bit
                 Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, CALCULATOR_RESULT);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode, data); // like get or fetch
+
+        if (requestCode == CALCULATOR_RESULT && resultCode == RESULT_OK && data != null){
+            if(data != null && data.hasExtra("Sum")){
+                double result = data.getDoubleExtra("Sum", 0.0);
+                Toast.makeText(this, "Result was " + result, Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
 }
